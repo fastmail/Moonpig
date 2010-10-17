6e2ac61551c582f749f698a1fb1530de0e0894d7
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2010-10-17 17:56:23 -0400

    started docs directory
    also some comments in Bank.pm of things-to-do

--- lib/Moonpig/Role/Bank.pm
+++ lib/Moonpig/Role/Bank.pm
@@ -8,12 +8,20 @@ use Moonpig::Types qw(Ledger Millicents);
 
 use namespace::autoclean;
 
+# The initial amount in the piggy bank
+# The amount *available* is this initial amount,
+# minus all the charges that transfer from this bank.
 has amount => (
   is  => 'ro',
   isa =>  Millicents,
   coerce   => 1,
   required => 1,
 );
+
+sub outstanding_balance {
+  # find all transactions targeting this piggy bank
+  # sum them, and deduct from the ->amount.
+}
 
 has ledger => (
   is   => 'ro',
