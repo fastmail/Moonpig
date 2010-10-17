6681482c8e60bcd2a6a6679e559655a3233ac822
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2010-10-17 16:27:35 -0400

    Invoice has date

--- lib/Moonpig/Role/Invoice.pm
+++ lib/Moonpig/Role/Invoice.pm
@@ -10,6 +10,15 @@ has cost_tree => (
   default  => sub { confess "we should really have a default cost tree" },
 );
 
+# TODO: make sure that charges added to this invoice have dates that
+# precede this date. 2010-10-17 mjd@icgroup.com
+has date => (
+  is  => 'ro',
+  required => 1,
+  default => sub { DateTime->now() },
+  isa => 'DateTime',
+}
+
 sub total_amt { confess "..."; }
 
 1;
