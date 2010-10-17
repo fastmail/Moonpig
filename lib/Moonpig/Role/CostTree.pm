2884a74e1152f8dfd21db6601d2c5556de5e32c1
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2010-10-17 17:16:26 -0400

    track the most recent date of all charges in a cost tree

--- lib/Moonpig/Role/CostTree.pm
+++ lib/Moonpig/Role/CostTree.pm
@@ -1,6 +1,8 @@
 package Moonpig::Role::CostTree;
 use Moose::Role;
 
+use DateTime;
+use List::Util qw(max);
 use Moose::Util::TypeConstraints;
 use MooseX::Types::Moose qw(ArrayRef HashRef);
 
@@ -24,6 +26,62 @@ has _parent => (
   does => 'Moonpig::Role::CostTree',
   predicate => '_has_parent',
 );
+
+# Root of the cost tree that this cost is in
+sub root {
+  my $this = shift;
+  $this = $this->_parent while $this->_has_parent;
+  return $this;
+}
+
+# Use this when adding a subtree to ensure that there are no loops
+# in the cost tree graph.
+sub _contains_cost_tree {
+  my ($self, $cost_tree, $seen) = @_;
+  return 0 if $seen->{$self}++;
+
+  return 1 if same_object($self, $cost_tree);
+  if (any { same_object($_, $cost_tree)
+              || $_->_contains_cost_tree($cost_tree, $seen) }
+      $self->subtrees) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+# This is the date of the latest item in the cost tree
+has last_date => (
+  is   => 'rw',
+  isa  => 'DateTime',
+  lazy => 1,
+  default => sub { $_[0]->_compute_last_date },
+);
+
+# This method recalculates the last date for the target object
+# and its ancestors.
+sub _compute_last_date {
+  my ($self) = @_;
+  my $last = max(0, map $_->date, $self->charges);
+  $last = max($last, map $_->last_date, $self->subtrees);
+  # if it changed, propagate the change to the parent
+  $self->_update_last_date($last);
+  return $last;
+}
+
+# A new charge with the specified date has been added somewhere below this
+# cost tree; update this object's ->last_date if necessary.
+# You must consider calling this whenever you add a charge or a
+# subtree to a cost tree.
+sub _update_last_date {
+  my ($self, $date) = @_;
+  if (_date_after($date, $self->last_date())) {
+    $self->last_date($date);
+    $self->_parent->_update_last_date($date) if $self->_has_parent;
+  }
+}
+
+sub _date_after { DateTime->compare(@_[0,1]) > 1 }
 
 sub find_or_create_path {
   my ($self, $path) = @_;
@@ -58,9 +116,22 @@ sub path_search {
   return     $next->path_search(\@rest, $opt);
 }
 
-has charges => (
+has _charges => (
   is  => 'ro',
   isa => ArrayRef[ role_type('Moonpig::Role::Charge') ],
+  default => sub { [] },
+  traits  => [ 'Array' ],
+  handles => {
+    charges  => 'elements',
+    n_charges => 'count',
+    _push_charge => 'push',
+  },
 );
+
+sub add_charge {
+  my ($self, $charge) = @_;
+  $self->_push_charge($charge);
+  $self->_update_last_date($charge->date);
+}
 
 1;
