6283dec8ced3e86110dbc0c9e0ee3f5552389b6f
Ricardo Signes <rjbs@cpan.org>
2010-10-22 19:31:35 -0400

    assert_to to get a safer sugar for coersion

--- lib/Moonpig/Util.pm
+++ lib/Moonpig/Util.pm
@@ -4,7 +4,9 @@ use warnings;
 use Carp qw(croak);
 use Scalar::Util qw(refaddr);
 
-use Sub::Exporter -setup => [ qw(dollars same_object) ];
+use Moonpig::Types ();
+use Moose::Util::TypeConstraints ();
+use Sub::Exporter -setup => [ qw(dollars same_object assert_to) ];
 
 sub dollars {
   my ($dollars) = @_;
@@ -14,14 +16,28 @@ sub dollars {
 }
 
 sub same_object {
-    my ($a, $b) = @_;
-    my $me = "Moonpig::Util::same_object";
-    @_ == 2 or croak(@_ . " arguments to $me");
-    my ($ra, $rb) = (refaddr $a, refaddr $b);
-    defined($ra) or croak("arg 1 to $me was not a reference");
-    defined($rb) or croak("arg 2 to $me was not a reference");
+  my ($a, $b) = @_;
+  my $me = "Moonpig::Util::same_object";
+  @_ == 2 or croak(@_ . " arguments to $me");
+  my ($ra, $rb) = (refaddr $a, refaddr $b);
+  defined($ra) or croak("arg 1 to $me was not a reference");
+  defined($rb) or croak("arg 2 to $me was not a reference");
 
-    $ra == $rb;
+  $ra == $rb;
+}
+
+# I really don't want to put this here, but I also really do not like the
+# MooseX::Types to_Foo behavior of returning false on failure.  Until we get an
+# assertive to, I will use this.
+sub assert_to {
+  my ($type, $value) = @_;
+  my $tc = Moose::Util::TypeConstraints::find_type_constraint(
+    'Moonpig::Types::' . $type
+  );
+  my $new_value = $tc->coerce($value);
+
+  $tc->assert_valid($new_value);
+  return $new_value;
 }
 
 1;
