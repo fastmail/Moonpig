01e0f6d33c910df00cc5a92cc5ba51acd2fbd5a0
Mark Dominus <mjd@icgroup.com>
2010-10-26 18:57:38 -0400

    add some attributes to Consumer::ByTime

--- lib/Moonpig/Role/Consumer/ByTime.pm
+++ lib/Moonpig/Role/Consumer/ByTime.pm
@@ -1,13 +1,55 @@
 package Moonpig::Role::Consumer::ByTime;
+use DateTime;
+use DateTime::Infinite;
 use Moose::Role;
+use namespace::autoclean;
 
 with(
   'Moonpig::Role::Consumer',
 );
 
-use Moonpig::Types qw(Ledger Millicents);
+use Moonpig::Types qw(Millicents);
 
-use namespace::autoclean;
+# How often I charge the bank, in days
+has charge_frequency => (
+  is => 'ro',
+  default => sub { 1 },
+  isa => 'Num',
+);
+
+# How much I cost to own, in millicents per period
+has cost_amount => (
+  is => 'ro',
+  required => 1,
+  isa => 'Num',
+);
+
+#  XXX this is period in days, which is not quite right, since a
+#  charge of $10 per month or $20 per year is not any fixed number of
+#  days, For example a charge of $20 annually, charged every day,
+#  works out to 5479 mc per day in common years, but 5464 mc per day
+#  in leap years.  -- 2010-10-26 mjd
+
+has cost_period => (
+   is => 'ro',
+   required => 1,
+   isa => 'Num',   # XXX in days
+);
+
+# I start generating low-balance events when my bank balance falls below this much
+has min_balance => (
+  is => 'ro',
+  isa => Millicents,
+  required => 1,
+);
+
+# Last time I charged the bank
+has last_charge_date => (
+  is => 'rw',
+  isa => 'DateTime',
+  default => sub { DateTime::Infinite::Past->new },
+);
+
 
 
 1;
--- t/consumer/bytime/basic.t
+++ t/consumer/bytime/basic.t
@@ -6,17 +6,46 @@ use Test::More;
 use Test::Routine::Util;
 use Moonpig::Consumer::ByTime;
 
+use Try::Tiny;
 
 with 't::lib::Factory::Ledger';
 
 
-# use Moonpig::Util -all;
+use Moonpig::Util -all;
+
+# todo: warning if no bank
+#       suicide
+#       create successor
+#       hand off to successor
 
 test "constructor" => sub {
   my ($self) = @_;
+  plan tests => 2;
+  my $ld = $self->test_ledger;
+
+  try {
+    # Missing attributes
+    Moonpig::Consumer::ByTime->new({ledger => $self->test_ledger});
+  } finally {
+    ok(@_, "missing attrs");
+  };
+
+  my $c = Moonpig::Consumer::ByTime->new({
+    ledger => $self->test_ledger,
+    cost_amount => dollars(1),
+    cost_period => 1,
+    min_balance => dollars(0),
+
+  });
+  ok($c);
+};
+
+test "warnings" => sub {
+  my ($self) = @_;
   plan tests => 1;
-  my $c0 = Moonpig::Consumer::ByTime->new({ledger => $self->test_ledger});
-  ok($c0);
+  my $ld = $self->test_ledger;
+  my $b = Moonpig::Bank::Basic->new({ledger => $ld, amount => 1000});
+  ok(1);
 };
 
 run_me;
