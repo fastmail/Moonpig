e28bed50fe35999212af8658d21ffe270009a368
Ricardo Signes <rjbs@cpan.org>
2010-10-28 16:10:34 -0400

    refactor common api from events/exceptions

--- lib/Moonpig/Events/Event.pm
+++ lib/Moonpig/Events/Event.pm
@@ -5,25 +5,8 @@ use Moonpig::Types qw(EventName);
 
 use namespace::autoclean;
 
-has payload => (
-  is  => 'ro',
-  isa => 'HashRef',
-  default => sub {  {}  },
-);
-
 with(
-  'Throwable::X::WithIdent',
-  'Throwable::X::WithTags',
-
-  'Throwable::X::WithMessage::Errf' => {
-    default  => sub { $_[0]->ident },
-    lazy     => 1,
-  },
-
-  'MooseX::OneArgNew' => {
-    type     => 'Throwable::X::_VisibleStr',
-    init_arg => 'ident',
-  },
+  'Moonpig::Role::Happening',
 );
 
 has '+ident' => (
--- lib/Moonpig/X.pm
+++ lib/Moonpig/X.pm
@@ -1,31 +1,9 @@
 package Moonpig::X;
 use Moose;
 
-# We do not use Throwable::X, because we want a way to provide manual payload
-# content for one-off exceptions without subclassing.  I will add that to
-# Throwable::X's composition. -- rjbs, 2010-10-27
-# with 'Throwable::X';
-
-has payload => (
-  is  => 'ro',
-  isa => 'HashRef',
-  default => sub {  {}  },
-);
-
 with(
   'Throwable',
-  'Throwable::X::WithIdent',
-  'Throwable::X::WithTags',
-
-  'Throwable::X::WithMessage::Errf' => {
-    default  => sub { $_[0]->ident },
-    lazy     => 1,
-  },
-
-  'MooseX::OneArgNew' => {
-    type     => 'Throwable::X::_VisibleStr',
-    init_arg => 'ident',
-  },
+  'Moonpig::Role::Happening',
 );
 
 has is_public => (
