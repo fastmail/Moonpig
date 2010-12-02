8c6bc40d19c78fa135ea2af44200f39252088fa0
Ricardo Signes <rjbs@cpan.org>
2010-12-02 11:30:58 -0500

    add holds

---
+++ lib/Moonpig/Hold.pm
@@ -0,0 +1,20 @@
+package Moonpig::Hold;
+use Moose;
+
+use Moonpig::Types qw(Millicents);
+
+use Moose::Util::TypeConstraints;
+
+use namespace::autoclean;
+
+with(
+  'Moonpig::Role::TransferLike' => {
+    from_name => 'bank',
+    from_type => role_type('Moonpig::Role::Bank'),
+
+    to_name   => 'consumer',
+    to_type   => role_type('Moonpig::Role::Consumer'),
+  },
+);
+
+1;
