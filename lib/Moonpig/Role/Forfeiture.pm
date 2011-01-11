e26446ab6db03cd2c7be663a7bf71e89b1640940
Ricardo Signes <rjbs@cpan.org>
2011-01-11 13:31:04 -0500

    seems likely we will want this later to kill off credit on canceled customers

---
+++ lib/Moonpig/Role/Forfeiture.pm
@@ -0,0 +1,27 @@
+package Moonpig::Role::Forfeiture;
+# ABSTRACT: a representation of a forfeit of a customer's credit
+use Moose::Role;
+
+with(
+  'Moonpig::Role::LedgerComponent',
+  'Moonpig::Role::HasGuid',
+  'Moonpig::Role::Payable',
+);
+
+use List::Util qw(reduce);
+
+use Moonpig::CreditApplication;
+
+use namespace::autoclean;
+
+sub amount {
+  my ($self) = @_;
+
+  my $xfers = Moonpig::CreditApplication->all_for_payable($self);
+
+  my $xfer_total = reduce { $a + $b } 0, (map {; $_->amount } @$xfers);
+
+  return $xfer_total;
+}
+
+1;
