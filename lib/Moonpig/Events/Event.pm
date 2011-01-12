c15592ed43aee7da3d04a58c43c9275c5942074c
Ricardo Signes <rjbs@cpan.org>
2011-01-12 11:04:30 -0500

    add a note about why Event is a class, not a role

--- lib/Moonpig/Events/Event.pm
+++ lib/Moonpig/Events/Event.pm
@@ -2,6 +2,10 @@ package Moonpig::Events::Event;
 # ABSTRACT: an event fired by one object for another to consume
 use Moose;
 
+# We had to make this a class, rather than a role, to +attr ident.  If we
+# really want, in the future, we can convert Happening into a parameterized
+# role. -- rjbs, 2011-01-12
+
 use Moonpig::Types qw(EventName);
 
 use namespace::autoclean;
