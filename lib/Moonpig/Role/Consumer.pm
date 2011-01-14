36dc3c9969685e9263c356cbe7fb0d972e48a1d8
Mark Dominus <mjd@icgroup.com>
2011-01-14 14:15:08 -0500

    Consumer requires construct_replacement

--- lib/Moonpig/Role/Consumer.pm
+++ lib/Moonpig/Role/Consumer.pm
@@ -40,6 +40,10 @@ sub unapplied_amount {
   return $self->has_bank ? $self->bank->unapplied_amount : 0;
 }
 
+# Should take an optional parameter hash, construct a replacement object
+# for this one, and install it in the ledger.
+requires 'construct_replacement';
+
 has replacement => (
   is   => 'rw',
   does => 'Moonpig::Role::Consumer',
