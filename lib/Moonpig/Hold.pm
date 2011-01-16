c9b01cb6ae09f4b7e954931fab2f6ee8531da07d
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2011-01-16 17:05:16 -0500

    Hold->commit and Hold->delete

--- lib/Moonpig/Hold.pm
+++ lib/Moonpig/Hold.pm
@@ -18,6 +18,22 @@ with(
   },
 );
 
-# also allow_deletion => 1
+has subsidiary_hold => (
+  is => 'rw',
+  isa => 'Moonpig::Hold',
+  predicate => 'has_subsidiary_hold',
+);
+
+before BUILD => sub {
+   my ($self, $arg) = @_;
+   $arg->{allow_deletion} = 1 unless exists $arg->{allow_deletion};
+   croak "Can't create immortal hold object"
+     unless $arg->{allow_deletion};
+}
+
+before delete => sub {
+  my ($self) = @_;
+  $self->subsidiary_hold->delete if $self->has_subsidiary_hold;
+};
 
 1;
