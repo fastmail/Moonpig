f4eed64c78eb968dddc462435e735ca8d2ed1658
Ricardo Signes <rjbs@cpan.org>
2011-01-19 13:55:10 -0500

    use env_prefix for Moonpig::Logger

--- lib/Moonpig/Logger.pm
+++ lib/Moonpig/Logger.pm
@@ -2,11 +2,23 @@ use strict;
 use warnings;
 package Moonpig::Logger;
 # ABSTRACT: the global Log::Dispatchouli logger for Moonpig
-use base 'Log::Dispatchouli::Global';
+use parent 'Log::Dispatchouli::Global';
+
+use Log::Dispatchouli 2.002 (); # env_prefix
 
 sub logger_globref {
   no warnings 'once';
   \*Logger;
+}
+
+sub default_logger_class { 'Moonpig::Logger::_Logger' }
+
+{
+  package
+    Moonpig::Logger::_Logger;
+  use parent 'Log::Dispatchouli';
+
+  sub env_prefix { 'MOONPIG' }
 }
 
 1;
