16992da31f951b1385f0618d1a30db49f6de47d5
Ricardo Signes <rjbs@cpan.org>
2011-02-03 13:32:13 -0500

    allow extra arguments in constructing test ledgers

--- t/lib/Factory/Ledger.pm
+++ t/lib/Factory/Ledger.pm
@@ -10,8 +10,9 @@ use Moonpig::Util -all;
 use namespace::autoclean;
 
 sub test_ledger {
-  my ($self, $class) = @_;
+  my ($self, $class, $extra) = @_;
   $class ||= class('Ledger');
+  $extra ||= {};
 
   my $contact = class('Contact')->new({
     name => 'J. Fred Bloggs',
@@ -19,6 +20,7 @@ sub test_ledger {
   });
 
   my $ledger = $class->new({
+    %$extra,
     contact => $contact,
   });
 
