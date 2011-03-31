f5e836ba0ee231ebbafbac3ce44cf8965fb9f48b
Ricardo Signes <rjbs@cpan.org>
2011-03-31 15:00:20 -0400

    improve "test routes" for moonpig::web
    1. add clock-related routes: now, advance-clock
    2. put into won subroutine

--- lib/Moonpig/Web/App.pm
+++ lib/Moonpig/Web/App.pm
@@ -12,6 +12,52 @@ use Plack::Request;
 
 my $JSON = JSON->new->ascii(1)->convert_blessed(1)->allow_blessed(1);
 
+sub test_routes {
+  my ($path, $storage) = @_;
+  my @path = @$path;
+
+  $storage->_reinstate_stored_time;
+
+  if (@path == 1 and $path[0] eq 'heartbeat-all') {
+    $storage->txn(sub {
+      my @guids = $storage->known_guids;
+      for my $guid (@guids) {
+        my $ledger = $storage->retrieve_ledger_for_guid($guid);
+        $ledger->handle_event( event('heartbeat') );
+        $storage->store_ledger($ledger);
+      }
+    });
+
+    return [
+      200,
+      [ 'Content-type' => 'application/json' ],
+      [ $JSON->encode({ result => 'heartbeats processed' }) ],
+    ];
+  }
+
+  if (@path == 1 and $path[0] eq 'time') {
+    return [
+      200,
+      [ 'Content-type' => 'application/json' ],
+      [ $JSON->encode({ now => Moonpig->env->now->epoch }) ],
+    ];
+  }
+
+  if (@path == 2 and $path[0] eq 'advance-clock') {
+    my $s = $path[1];
+    Moonpig->env->stop_clock_at( Moonpig->env->now + $s );
+    Moonpig->env->restart_clock;
+    $storage->_store_time;
+    $storage->_reinstate_stored_time;
+
+    return [
+      200,
+      [ 'Content-type' => 'application/json' ],
+      [ $JSON->encode({ now => Moonpig->env->now->epoch }) ],
+    ];
+  }
+}
+
 sub app {
   return sub {
     my ($env) = @_;
@@ -23,23 +69,11 @@ sub app {
     shift @path; # get rid of leading "/" part
 
     my $response = try {
-      if (1) { # XXX: IF WE ARE IN TESTING MODE -- rjbs, 2011-03-30
-        if (@path == 1 and $path[0] eq 'heartbeat-all') {
-          $storage->txn(sub {
-            my @guids = $storage->known_guids;
-            for my $guid (@guids) {
-              my $ledger = $storage->retrieve_ledger_for_guid($guid);
-              $ledger->handle_event( event('heartbeat') );
-              $storage->store_ledger($ledger);
-            }
-          });
 
-          return [
-            200,
-            [ 'Content-type' => 'application/json' ],
-            [ $JSON->encode({ result => 'heartbeats processed' }) ],
-          ];
-        }
+      # XXX: IF WE ARE IN TESTING MODE -- rjbs, 2011-03-30
+      if (1) {
+        my $res = test_routes(\@path, $storage);
+        return $res if $res;
       }
 
       my $resource = Moonpig->env->route(\@path);
