e386d2ebf4481af247cd824a07f26216c32d5a56
Mark Dominus <mjd@icgroup.com>
2011-05-04 14:16:02 -0400

    ConsumerExtras requires publish that doesn't rename "http_method" to "method"

--- lib/Moonpig/Role/Collection/ConsumerExtras.pm
+++ lib/Moonpig/Role/Collection/ConsumerExtras.pm
@@ -2,7 +2,7 @@ package Moonpig::Role::Collection::ConsumerExtras;
 use Moose::Role;
 use MooseX::Types::Moose qw(Str HashRef);
 use Stick::Publisher 0.20110324;
-use Stick::Publisher::Publish 0.20110324;
+use Stick::Publisher::Publish 0.20110504;
 
 publish add_from_template => { -http_method => 'post',
                                -path => 'add_from_template',
