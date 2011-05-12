ac21374cd4ae886cddd59b72c3c9092abe1de0d9
Ricardo Signes <rjbs@cpan.org>
2011-05-12 15:47:41 -0400

    add ->account method for Account consumers in fauxbox

--- eg/fauxbox/lib/Fauxbox/Moonpig/Consumer/Account.pm
+++ eg/fauxbox/lib/Fauxbox/Moonpig/Consumer/Account.pm
@@ -6,6 +6,19 @@ with(
 
 use namespace::autoclean;
 
+use Fauxbox::Schema;
 
+sub account {
+  my ($self) = @_;
+
+  my $xid = $self->xid;
+  my ($account_id) = $xid =~ /\A fauxbox:account: ( [0-9]+ ) \z/x;
+
+  my $account = Fauxbox::Schema
+              ->shared_connection
+              ->resultset('Account')->find( $account_id );
+
+  return $account;
+}
 
 1;
