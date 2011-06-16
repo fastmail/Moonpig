70a93392f46f2211cf4b7da9ef1955d7271c8d22
Ricardo Signes <rjbs@cpan.org>
2011-06-16 16:18:49 -0400

    first pass at ChargeTreeContainer replacement

---
+++ lib/Moonpig/Role/HasCharges.pm
@@ -0,0 +1,86 @@
+package Moonpig::Role::HasCharges;
+# ABSTRACT: something that has a set of charges associated with it
+use MooseX::Role::Parameterized;
+
+use namespace::autoclean;
+
+use Moonpig;
+
+use Moonpig::Types qw(Charge);
+use Moonpig::Util qw(class);
+use MooseX::Types::Moose qw(ArrayRef);
+use Stick::Types qw(StickBool);
+use Stick::Util qw(true false);
+
+parameter charges_handle_events => (
+  isa      => 'Bool',
+  required => 1,
+);
+
+role {
+  my $p = shift;
+
+  has charges => (
+    is  => 'ro',
+    isa => ArrayRef[ Charge ],
+    init_arg => undef,
+    default  => sub {  []  },
+    traits   => [ 'Array' ],
+    handles  => {
+      all_charges => 'elements',
+      _add_charge => 'push',
+    },
+  );
+
+  method add_charge   => sub { ... };
+  method total_amount => sub { ... };
+  method all_charges  => sub { ... };
+
+  method _objectify_charge => sub {
+    my ($self, $input) = @_;
+    return $input if blessed $input;
+
+    my $class = $p->charges_handle_events
+              ? class('Charge::HandlesEvents')
+              : class('Charge');
+
+    $class->new($input);
+  };
+
+  method add_charge => sub {
+    my ($self, $charge_input) = @_;
+
+    my $charge = $self->_objectify_charge( $charge_input );
+
+    my $handles = $charge->does('Moonpig::Role::HandlesEvents');
+
+    Moonpig::X->throw("bad charge type")
+      if $handles xor $p->charges_handle_events;
+
+    $self->_add_charge($charge);
+
+    return $charge;
+  };
+
+  has closed => (
+    isa     => StickBool,
+    coerce  => 1,
+    default => 0,
+    reader  => 'is_closed',
+    writer  => '__set_closed',
+  );
+
+  method close   => sub { $_[0]->__set_closed( true ) };
+  method is_open => sub { ! $_[0]->is_closed };
+
+  # TODO: make sure that charges added to this container have dates that
+  # precede this date. 2010-10-17 mjd@icgroup.com
+  has date => (
+    is  => 'ro',
+    required => 1,
+    default => sub { Moonpig->env->now() },
+    isa => 'DateTime',
+  );
+};
+
+1;
