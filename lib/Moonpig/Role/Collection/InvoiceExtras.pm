6f605beffa6e1d7ec02399b69dc06fed8ea5d06d
Ricardo Signes <rjbs@cpan.org>
2011-06-23 11:32:37 -0400

    we want unpaid invoices, not open ones

--- lib/Moonpig/Role/Collection/InvoiceExtras.pm
+++ lib/Moonpig/Role/Collection/InvoiceExtras.pm
@@ -4,9 +4,9 @@ use MooseX::Types::Moose qw(Str HashRef);
 use Stick::Publisher 0.20110324;
 use Stick::Publisher::Publish 0.20110504;
 
-publish open => {} => sub {
+publish unpaid => {} => sub {
   my ($self) = @_;
-  return grep {; $_->is_open } @{ $self->items };
+  return [ grep {; $_->is_unpaid } @{ $self->items } ];
 };
 
 1;
