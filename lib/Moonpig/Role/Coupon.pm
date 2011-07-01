ff38928016266f13fc0660259296e90c982fb362
Mark Dominus <mjd@icgroup.com>
2011-07-01 17:19:30 -0400

    rough out Role::Coupon and Role::Coupon::Simple

---
+++ lib/Moonpig/Role/Coupon.pm
@@ -0,0 +1,55 @@
+package Moonpig::Role::Coupon;
+# ABSTRACT: a discount for paying for a certain service
+use Moose::Role;
+use Moonpig;
+use Moonpig::Types qw(Factory Time);
+use Moonpig::Util qw(class);
+
+with(
+  'Moonpig::Role::HasGuid',
+  'Moonpig::Role::LedgerComponent',
+);
+
+use namespace::autoclean;
+
+has description => (
+  is => 'ro',
+  isa => 'Str',
+  required => 1,
+);
+
+has expiration_date => (
+  is => 'ro',
+  isa => Time,
+  predicate => 'has_expiration_date',
+);
+
+has credit_class => (
+  is => 'ro',
+  isa => 'Str',
+  default => class('Credit::Discount'),
+);
+
+sub is_expired {
+  my ($self) = @_;
+  $self->has_expiration_date
+    and $self->expiration_date->precedes(Moonpig->env->now);
+}
+
+requires 'applies_to';
+requires 'discount_amount_for';
+
+sub create_discount_for {
+  my ($self, $charge) = @_;
+  return if $self->is_expired;
+  return unless $self->applies_to($charge);
+
+  my $amount = $self->discount_amount_for($charge);
+  return if $amount == 0;
+
+  return $self->ledger->add_credit(
+    $self->credit_class,
+    { amount => $amount });
+}
+
+1;
---
+++ lib/Moonpig/Role/Coupon/Simple.pm
@@ -0,0 +1,38 @@
+package Moonpig::Role::Coupon::Simple;
+# ABSTRACT: a disount for paying for a certain servive
+use Moose::Role;
+
+use List::Util qw(min);
+use Moonpig::Types qw(NonNegativeMillicents Time);
+use Moonpig;
+use Moose::Util::TypeConstraints;
+use MooseX::Types::Moose qw(Num);
+
+with(
+  'Moonpig::Role::LedgerComponent',
+);
+
+use namespace::autoclean;
+
+# If the deal is that you get the service at 15% off, then this should be 0.15.
+has discount_rate => (
+  is => 'ro',
+  isa => subtype(Num, { where => sub { 0 <= $_ && $_ <= 1 } }),
+  default => 0,
+);
+
+# If the deal is that you get the service at $3.79 off, then this should be cents(379).
+has flat_discount_amount => (
+  is => 'ro',
+  isa => NonNegativeMillicents,
+  default => 0,
+);
+
+sub discount_amount_for {
+  my ($self, $charge) = @_;
+  my $charge_amount = $charge->amount;
+  my $discount = $charge_amount * $self->discount_rate + $self->flat_discount_amount;
+  return min($discount, $charge_amount);
+}
+
+1;
