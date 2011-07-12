bb43bd9912faa395e4f60d6e92b3abf0653eed1d
Mark Dominus <mjd@icgroup.com>
2011-07-12 16:31:39 -0400

    rework test factories

--- t/lib/Factory/Ledger.pm
+++ t/lib/Factory/Ledger.pm
@@ -1,6 +1,6 @@
 package t::lib::Factory::Ledger;
-use Moose::Role;
 
+use Carp qw(confess croak);
 use Data::GUID qw(guid_string);
 
 use Moonpig::Env::Test;
@@ -9,22 +9,91 @@ use Moonpig::Util -all;
 
 use namespace::autoclean;
 
-sub test_ledger {
-  my ($self, $class, $args) = @_;
-  $class ||= class('Ledger');
-  $args ||= {};
+sub build {
+  my ($self, %args) = @_;
+  my %stuff;
 
-  my $contact = class('Contact')->new({
-    name => 'J. Fred Bloggs',
-    email_addresses => [ 'jfred@example.com' ],
-  });
+  $stuff{ledger} = $self->build_ledger($args{ledger});
+  delete $args{ledger};
 
-  my $ledger = $class->new({
-    contact => $contact,
-    %$args,
-  });
+  $self->build_consumers(\%args, \%stuff);
 
-  return $ledger;
+  return \%stuff;
+}
+
+sub build_ledger {
+  my ($self, $args) = @_;
+  my %args = %{$args || {}};
+  my $class = delete $args{class} || class('Ledger');
+  $args{contact} ||= $self->build_contact;
+  return $class->new(\%args);
+}
+
+sub build_consumers {
+  my ($self, $args, $stuff) = @_;
+
+  my %name_by_guid; # backwards mapping from guid of created consumer to name
+  # create all required consumers
+  for my $c_name (keys %$args) {
+    next if exists $stuff->{$c_name};
+    my %c_args = %{$args->{$c_name}};
+    $stuff->{$c_name} = $self->build_consumer($c_name, \%c_args, $stuff);
+    $name_by_guid{$stuff->{$c_name}->guid} = $c_name;
+  }
+
+  # find the ones that are *not* replacements and activate them unless otherwise specified
+  { my %consumer = map { $stuff{$_}->guid => $stuff{$_} } keys %args;
+    # delete all the consumers that are replacements
+    for my $consumer (values %consumer) {
+      $consumer->replacement && delete $consumer{$consumer->replacement->guid};
+    }
+    # iterate over non-replacements, activating each
+    for my $consumer (values %consumer) {
+      my $name = $name_by_guid{$consumer_guid};
+      # activate by default, or if the arg value is true
+      if (! exists $args->{$name}{make_active} || $args->{$name}{make_active}) {
+        $consumer->make_active;
+      }
+    }
+  }
+}
+
+sub build_consumer {
+  my ($self, $name, $args, $stuff) = @_;
+  my $become_active = delete $args->{make_active};
+
+  # If this consumer will have a replacement, build that first
+  my $replacement_name = $args->{replacement};
+  if (defined($replacement_name) && ! exists $stuff->{$replacement_name}) {
+    $args->{replacement} =  $self->build_consumer($replacement_name, $args, $stuff);
+  }
+
+  my $bank;
+  if (exists $args->{bank} && $args->{bank} > 0) {
+    $args->{bank} = $self->build_bank({ amount => $args->{bank} }, $stuff);
+  }
+
+  my $class = delete $args->{class}
+    or croak "Arguments for consumer '$name' have no 'class'\n";
+  $class = "Consumer::$class" unless $class =~ /^Consumer::/;
+  my $consumer = $stuff->{ledger}->add_consumer(
+    class($class),
+    { charge_tags => [],
+      xid => "test:consumer:$name",
+      %$args,
+    });
+
+  $consumer->become_active if $become_active;
+
+  return $consumer;
+}
+
+sub build_bank {
+  my ($self, $args, $stuff) = @_;
+
+  return $stuff->{ledger}->add_bank(
+    class("Bank"),
+    { amount => $args->{amount} });
 }
 
 sub rnd {
@@ -32,7 +101,7 @@ sub rnd {
   return $items[int(rand(1000)) % @items];
 }
 
-sub random_contact {
+sub build_contact {
   my ($self) = @_;
   my @first = qw(John Mary William Anna James Margaret George Helen Charles Elizabeth);
   my @last = qw(Smith Johnson Williams Jones Brown Davis Miller Wilson Moore Taylor);
@@ -40,48 +109,8 @@ sub random_contact {
   my $inits = join "", map substr($_, 0, 1), @names;
   return class('Contact')->new({
     name => join(" ", @names),
-    email_addresses => [ "\L$inits\@example.com" ],
+    email_addresses => [ "\L$inits\E\@example.com" ],
   });
-}
-
-sub add_bank_to {
-  my ($self, $ledger, $args) = @_;
-
-  my $bank = $ledger->add_bank(
-    class(qw(Bank)),
-    {
-      amount => $args->{amount} || dollars(100),
-    }
-  );
-
-  return $bank;
-}
-
-sub add_consumer_to {
-  my ($self, $ledger, $args) = @_;
-  my $class = delete $args->{class} || class("Consumer::Dummy");
-
-  my $consumer = $ledger->add_consumer(
-    $class,
-    {
-      xid             => 'urn:uuid:' . guid_string,
-      make_active     => 1,
-      replacement_mri => Moonpig::URI->nothing(),
-      %$args,
-    },
-  );
-
-  return $consumer;
-}
-
-sub add_bank_and_consumer_to {
-  my ($self, $ledger, $args) = @_;
-  $args ||= {};
-
-  my $bank = $self->add_bank_to($ledger, $args);
-  my $consumer = $self->add_consumer_to($ledger, {%$args, bank => $bank});
-
-  return ($bank, $consumer);
 }
 
 1;
