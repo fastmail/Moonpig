a063033c986b6a8601451d351763e35be9830fcd
Ricardo Signes <rjbs@cpan.org>
2011-08-08 13:33:56 -0400

    move the meat of class() to MooseX::ClassCompositor

--- lib/Moonpig/Util.pm
+++ lib/Moonpig/Util.pm
@@ -10,7 +10,7 @@ use Moonpig::Events::Event;
 use Moose::Util::TypeConstraints ();
 
 use Carp qw(croak);
-use Memoize;
+use MooseX::ClassCompositor;
 use MooseX::StrictConstructor::Trait::Class;
 use Moose::Util::MetaRole ();
 use Scalar::Util qw(refaddr);
@@ -32,118 +32,27 @@ use Sub::Exporter -setup => [ qw(
   sum
 ) ];
 
-memoize(class => (NORMALIZER => \&make_key,
-                  LIST_CACHE => 'MERGE',
-                 ));
+my $COMPOSITOR = MooseX::ClassCompositor->new({
+  class_basename  => 'Moonpig::Class',
+  class_metaroles => {
+    class => [ 'MooseX::StrictConstructor::Trait::Class' ],
+  },
+  role_prefixes   => {
+   ''    => 'Moonpig::Role::',
+   '='   => '',
+   't::' => 't::lib::Role::',
+  }
+});
 
 use Moose::Util qw(apply_all_roles);
 # Arguments here are role names, or role objects followed by nonce-names.
 
-my %CLASS_ROLES;
-
-my $SERIAL = "AA";
-
 sub class {
-  my (@args) = @_;
-  my @orig_args = @args;
-
-  # $role_hash is a hash mapping nonce-names to role objects
-  # $role_names is an array of names of more roles to add
-  my (@roles, @role_class_names, @all_names);
-
-  while (@args) {
-    my $name = shift @args;
-    if (ref $name) {
-      my ($role_name, $moniker, $params) = @$name;
-
-      my $full_name = _rewrite_prefix($role_name);
-      Class::MOP::load_class($full_name);
-      my $role_object = $full_name->meta->generate_role(
-        parameters => $params,
-      );
-
-      push @roles, $role_object;
-      $name = $moniker;
-    } else {
-      push @role_class_names, $name;
-    }
-
-    $name =~ s/::/_/g if @all_names;
-    $name =~ s/^=//;
-
-    push @all_names, $name;
-  }
-
-  my $name = join q{::}, 'Moonpig::Class', @all_names;
-
-  @role_class_names = _rewrite_prefix(@role_class_names);
-
-  Class::MOP::load_class($_) for @role_class_names;
-
-  if ($name->can('meta')) {
-    $name .= "_" . $SERIAL++;
-  }
-  my $class = Moose::Meta::Class->create( $name => (
-    superclasses => [ 'Moose::Object' ],
-  ));
-
-  apply_all_roles($class, @role_class_names, map $_->name, @roles);
-
-  $class = Moose::Util::MetaRole::apply_metaroles(
-    for => $class->name,
-    class_metaroles => {
-      class => [ 'MooseX::StrictConstructor::Trait::Class' ],
-    },
-  );
-
-  $class->make_immutable;
-
-  $CLASS_ROLES{ $name } = \@orig_args;
-
-  return $class->name;
-}
-
-sub make_key {
-  my @args = @_;
-  my @k;
-  while (@args) {
-    my $arg = shift @args;
-    if (ref $arg) {
-      my ($role_name, $moniker, $params) = @$arg;
-      push @k, "$moniker : { " . hash_to_string($params) . " }";
-    } else {
-      push @k, $arg;
-    }
-  }
-  my $key = join "; ", @k;
-  return $key;
-}
-
-sub hash_to_string {
-  my ($h) = @_;
-  my @k;
-  for my $k (sort keys %$h) {
-    my $v = ! defined($h->{$k}) ? "<undef>" :
-              ref($h->{$k}) ? join("-", @{$h->{$k}}) : $h->{$k};
-    push @k, "$k => $v";
-  }
-  join ", " => @k;
-}
-
-sub _rewrite_prefix {
-  my (@in) = @_;
-  return String::RewritePrefix->rewrite(
-    {
-     ''    => 'Moonpig::Role::',
-     '='   => '',
-     't::' => 't::lib::Role::',
-    },
-    @in
-  );
+  $COMPOSITOR->class_for(@_);
 }
 
 sub class_roles {
-  return \%CLASS_ROLES;
+  return { $COMPOSITOR->known_classes };
 }
 
 sub event {
