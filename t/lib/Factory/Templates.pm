c989ad08c353bb1b2daca5b3a948f4196d036e34
Mark Dominus <mjd@icgroup.com>
2011-08-09 17:06:30 -0400

    "quick" test consumer template: charges $100 over 2 days

--- t/lib/Factory/Templates.pm
+++ t/lib/Factory/Templates.pm
@@ -14,6 +14,7 @@ use namespace::autoclean;
 sub templates {
   my $dummy_xid = "consumer:test:dummy";
   my $b5g1_xid = "consumer:5y:test";
+
   return {
     dummy => sub {
       my ($name) = @_;
@@ -36,6 +37,20 @@ sub templates {
         },
       }
     },
+
+    quick => sub {
+      my ($name) = @_;
+      return {
+        roles => [ 'Consumer::ByTime::FixedCost' ],
+        arg => {
+          old_age     => years(1000),
+          cost_amount => dollars(100),
+          cost_period => days(2),
+          charge_description => 'quick consumer',
+          replacement_mri => Moonpig::URI->nothing(),
+        }};
+    },
+
     fiveyear => sub {
       my ($name) = @_;
 
