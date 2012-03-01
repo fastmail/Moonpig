697e20a8bca2c714e27e54165aa597987c47700c
Ricardo Signes <rjbs@cpan.org>
2012-03-01 14:19:35 -0500

    replacement_chain_expiration_date method

--- lib/Moonpig/Role/Consumer/PredictsExpiration.pm
+++ lib/Moonpig/Role/Consumer/PredictsExpiration.pm
@@ -3,8 +3,26 @@ use Moose::Role;
 
 use namespace::autoclean;
 
+require Stick::Publisher;
+Stick::Publisher->VERSION(0.20110324);
+use Stick::Publisher::Publish 0.20110324;
+
+use List::AllUtils qw(any);
+use Moonpig::Util qw(sumof);
+
 requires 'estimated_lifetime'; # TimeInterval, from created to predicted exp
 requires 'expiration_date';    # Time, predicted exp date
 requires 'remaining_life';     # TimeInterval, from now to predicted exp
+
+publish replacement_chain_expiration_date => {} => sub {
+  my ($self) = @_;
+
+  my @chain = $self->replacement_chain;
+  if (any {! $_->does('Moonpig::Role::Consumer::PredictsExpiration')} @chain) {
+    Moonpig::X->throw("replacement in chain cannot predict expiration");
+  }
+
+  return($self->expiration_date + (sumof { $_->estimated_lifetime } @chain));
+};
 
 1;
