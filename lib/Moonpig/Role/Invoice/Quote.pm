09cc7edf2fd437ef4757af47f5ca3703b47bc6fe
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2012-03-23 11:57:02 -0400

    Quote role

---
+++ lib/Moonpig/Role/Invoice/Quote.pm
@@ -0,0 +1,29 @@
+package Moonpig::Role::Quote;
+# ABSTRACT: like an invoice, but doesn't expect to be paid
+
+use Moonpig::Types qw(Time);
+use Moose::Role;
+use MooseX::SetOnce;
+
+with(
+  'Moonpig::Role::Invoice'
+);
+
+requires qw(is_quote is_invoice);
+
+# XXX better name here
+has promoted_at => (
+  is   => 'ro',
+  isa  => Time,
+  traits => [ qw(SetOnce) ],
+  predicate => 'is_promoted',
+);
+
+has expiration_time => (
+  is => 'rw',
+  isa => Time,
+  predicate => 'has_expiration_time',
+);
+
+1;
+
