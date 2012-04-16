a39d3d7b0b594f92411371c95459df4c41525563
Ricardo Signes <rjbs@cpan.org>
2012-04-16 16:18:00 -0400

    allow quote_guid to be passed with credit addition
    This is *CLEARLY* a bogus hack.  This is one of the places where we
    clearly want the request multiplexer for Stick that we had talked about
    in the past.  Still, we don't have it yet, and it's not going to get
    designed and added and tested at this late date for deployment.  When we
    add it later, this one stupid exception can be removed.

--- lib/Moonpig/Role/Collection/CreditExtras.pm
+++ lib/Moonpig/Role/Collection/CreditExtras.pm
@@ -6,11 +6,23 @@ use Moonpig::Util qw(class);
 use Stick::Publisher 0.20110324;
 use Stick::Publisher::Publish 0.20110504;
 
+my %OK_ADD_ARG = map {; $_ => 1 } qw(type attributes quote_guid);
+
 sub add {
   my ($self, $arg) = @_;
   my $type = $arg->{type};
 
+  my @unknown = grep { ! $OK_ADD_ARG{ $_ } } keys %$arg;
+  Moonpig::X->throw("unknown arguments: @unknown") if @unknown;
+
   return Moonpig->env->storage->do_rw(sub {
+    if ($arg->{quote_guid}) {
+      my $quote = $self->owner->invoice_collection->find_by_guid({
+        guid => $arg->{quote_guid},
+      });
+      $quote->execute;
+    }
+
     my $credit = $self->owner->add_credit(
       class("Credit::$type"),
       $arg->{attributes},
