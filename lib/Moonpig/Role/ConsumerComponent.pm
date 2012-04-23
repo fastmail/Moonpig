da23c6a53ebb016c17c487c5fcc97497894d6ea0
Mark Dominus <mjd@icgroup.com>
2012-04-23 15:46:51 -0400

    break out InvoiceCharge owned-by-consumer stuff into a role

---
+++ lib/Moonpig/Role/ConsumerComponent.pm
@@ -0,0 +1,53 @@
+package Moonpig::Role::ConsumerComponent;
+# ABSTRACT: something that is owned by a consumer
+use Moose::Role;
+with ("Moonpig::Role::StubBuild");
+
+use namespace::autoclean;
+use Moonpig::Types qw(GUID);
+
+# translate ->new({consumer => ... }) to
+# ->new({ ledger_guid => ..., owner_guid => ... })
+around BUILDARGS => sub {
+  my $orig  = shift;
+  my $class = shift;
+  my $args = shift;
+
+  if (my $consumer = delete $args->{consumer}) {
+    $args->{owner_guid}  ||= $consumer->guid;
+    $args->{ledger_guid} ||= $consumer->ledger->guid;
+  }
+
+  return $class->$orig($args, @_);
+};
+
+has owner_guid => (
+  is   => 'ro',
+#  does => 'Moonpig::Role::Consumer',
+  isa => GUID,
+  required => 1,
+);
+
+sub owner {
+  my ($self) = @_;
+  $self->ledger->consumer_collection
+    ->find_by_guid({ guid => $self->owner_guid });
+}
+
+has ledger_guid => (
+  is   => 'ro',
+  isa => GUID,
+  required => 1,
+);
+
+sub ledger {
+  my ($self) = @_;
+  my $ledger = Moonpig->env->storage->retrieve_ledger_for_guid(
+    $self->ledger_guid
+  );
+
+  return($ledger || Moonpig::X->throw("couldn't find ledger for charge"));
+}
+
+1;
+
--- lib/Moonpig/Role/InvoiceCharge.pm
+++ lib/Moonpig/Role/InvoiceCharge.pm
@@ -4,48 +4,14 @@ use Moose::Role;
 
 with(
   'Moonpig::Role::Charge',
+  'Moonpig::Role::ConsumerComponent',
   'Moonpig::Role::HandlesEvents',
 );
 
 use namespace::autoclean;
 use Moonpig::Behavior::EventHandlers;
-use Moonpig::Types qw(GUID Time);
+use Moonpig::Types qw(Time);
 use MooseX::SetOnce;
-
-# translate ->new({consumer => ... }) to
-# ->new({ ledger_guid => ..., owner_guid => ... })
-around BUILDARGS => sub {
-  my $orig  = shift;
-  my $class = shift;
-  my $args = shift;
-
-  if (my $consumer = delete $args->{consumer}) {
-    $args->{owner_guid}  ||= $consumer->guid;
-    $args->{ledger_guid} ||= $consumer->ledger->guid;
-  }
-
-  return $class->$orig($args, @_);
-};
-
-has owner_guid => (
-  is   => 'ro',
-#  does => 'Moonpig::Role::Consumer',
-  isa => GUID,
-  required => 1,
-);
-
-sub owner {
-  my ($self) = @_;
-  $self->ledger->consumer_collection
-    ->find_by_guid({ guid => $self->owner_guid });
-}
-
-has ledger_guid => (
-  is   => 'ro',
-#  does => 'Moonpig::Role::Ledger',
-  isa => GUID,
-  required => 1,
-);
 
 has abandoned_date => (
   is => 'ro',
@@ -60,15 +26,6 @@ sub counts_toward_total { ! $_[0]->is_abandoned }
 sub mark_abandoned {
   my ($self) = @_;
   $self->__set_abandoned_date( Moonpig->env->now );
-}
-
-sub ledger {
-  my ($self) = @_;
-  my $ledger = Moonpig->env->storage->retrieve_ledger_for_guid(
-    $self->ledger_guid
-  );
-
-  return($ledger || Moonpig::X->throw("couldn't find ledger for charge"));
 }
 
 has executed_at => (
