0d59ac49cbe8c6661eb76baae461520b324fbcd7
Ricardo Signes <rjbs@cpan.org>
2012-04-30 14:51:53 -0400

    stop using grace_until, which should be a private attr
    instead, use grace_period_duration, which is publicker

--- t/lib/ConsumerTemplateSet/Demo.pm
+++ t/lib/ConsumerTemplateSet/Demo.pm
@@ -14,13 +14,13 @@ sub templates {
       return {
         roles => [ '=t::lib::Role::Consumer::ByTime::NFixedAmountCharges' ],
         arg   => {
-          charge_amounts       => [ dollars(40), dollars(10) ],
+          charge_amounts     => [ dollars(40), dollars(10) ],
           cost_period        => years(1),
           charge_frequency   => days(7), # less frequent to make logs simpler
           charge_description => 'yoyodyne service',
-          replacement_lead_time            => days(30),
           extra_charge_tags  => [ 'yoyodyne.basic' ],
-          grace_until        => Moonpig->env->now + days(3),
+          replacement_lead_time => days(30),
+          grace_period_duration => days(3),
 
           replacement_plan   => [ get => '/consumer-template/demo-service' ],
         },
