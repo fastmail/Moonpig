18f8fbbddd0f9046a258ce8de2cab5b2511e55b6
Mark Dominus <mjd@icgroup.com>
2012-07-09 17:08:49 -0400

    psync charges for b5g1 consumers
    The "charge" is actually a magic item which, if paid, notifies the
    consumer to adjust the amount by which it will fund itself when it
    becomes active.

--- lib/Moonpig/Role/Consumer/SelfFunding.pm
+++ lib/Moonpig/Role/Consumer/SelfFunding.pm
@@ -75,6 +75,40 @@ around _invoice => sub {
   return;
 };
 
+# This completely overrides the behavior in ByTime -- mjd, 2012-07-09
+around _issue_psync_charge => sub {
+  my (undef, $self, @args) = @_;
+
+  # If I am the active consumer, no additional funding is required
+  return if $self->is_active;
+
+  my $quote = $self->ledger->current_invoice;
+  $quote->is_quote && $quote->is_psync_quote
+    or die "Current journal is not a psync quote!";
+
+  my @ancestors = $self->_previous_n_ancestors(5);
+  my %is_ancestor = map { $_->guid => 1 } @ancestors;
+
+  my @ancestor_charges = grep { $is_ancestor{$_->owner->guid} } $quote->all_charges;
+  return unless @ancestor_charges;
+  my $average_charge_amount = (sumof { $_->amount } @ancestor_charges) / @ancestor_charges;
+
+  my $line_item = class("LineItem::PsyncB5G1Magic");
+  $self->charge_current_invoice($line_item);
+};
+
+sub _previous_n_ancestors {
+  my ($self, $n) = @_;
+
+  my $root = $self->ledger->active_consumer_for_xid($self->xid);
+  # get my ancestors
+  my @chain = grep $_->replacement_chain_contains($self) && $_->guid ne $self->guid,
+    $root, $root->replacement_chain;
+  # Throw away all but the last $n of them, if requested
+  if (defined $n) { shift @chain while @chain > $n }
+  return @chain;
+}
+
 PARTIAL_PACK {
   return {
     self_funding_amount => $_[0]->self_funding_amount,
