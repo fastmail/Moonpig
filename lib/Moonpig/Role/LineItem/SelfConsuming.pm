8afac63735f02f891e6f9460ae8c0a8dd4b9f318
Ricardo Signes <rjbs@cpan.org>
2014-07-30 11:51:47 -0400

    line item role for one-time charges

---
+++ lib/Moonpig/Role/LineItem/SelfConsuming.pm
@@ -0,0 +1,24 @@
+package Moonpig::Role::LineItem::SelfConsuming;
+# ABSTRACT: a magic line item that consumes its funds immediately on payment
+use Moonpig;
+use Moonpig::Types qw(NonNegativeMillicents);
+
+use Moose::Role;
+
+with ('Moonpig::Role::LineItem',
+      'Moonpig::Role::LineItem::Active',
+      'Moonpig::Role::LineItem::Abandonable',
+     );
+
+sub when_paid {
+  my ($self) = @_;
+
+  my $owner = $self->owner;
+  $owner->charge_current_journal({
+    description => $self->description,
+    amount      => $self->amount,
+    skip_funds_check => $owner->allows_overdrafts,
+  });
+}
+
+1;
