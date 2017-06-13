02368c1e1ec6016ff3fa5b6247bf7e19655ce319
Ricardo Signes <rjbs@cpan.org>
2017-06-13 16:19:11 -0400

    remove one last dead mention of Class::MOP

--- lib/Moonpig/Test/Factory.pm
+++ lib/Moonpig/Test/Factory.pm
@@ -6,7 +6,6 @@ package Moonpig::Test::Factory;
 use Moonpig::Test::Factory::Templates; # default testing template set
 
 use Carp qw(confess croak);
-use Class::MOP ();
 use Data::GUID qw(guid_string);
 use Scalar::Util qw(blessed);
 
